75cf6bdf4a3b5481c4b9e2a77ca6cc2c473104aa
Tam Nguyen <tamnt@zigexn.vn>
2015-05-19 15:14:35 +0700

    update home page

---
+++ wp-content/themes/vemaybayvietjetgiare/_hinh-thuc-dat-ve.php
@@ -0,0 +1,82 @@
+<h2>các hình thức đặt vé</h2>
+<div class="col-md-12 col-xs-12">
+  <div class="title-main">các hình thức <span>đặt vé</span>
+  </div>
+  <div class="border-title"></div>
+
+  <div class="row form-group">
+    <div class="col-md-12">
+      <div class="icon-datve"><i class="fa fa-globe"></i></div>
+      <div class="text-chu-thic">
+        <div class="text-big">Đặt vé trực tiếp trên website.</div>
+        <div class="text-small">Sẽ được nhân viên xác nhận sau vài phút</div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row form-group">
+    <div class="col-md-12">
+      <div class="icon-datve"><i class="fa fa-comments"></i></div>
+      <div class="text-chu-thic">
+        <div class="text-big">Qua chat Yahoo hoặc chat trên website</div>
+        <div class="text-small">
+
+        <div class="yahoo">
+            <a href="ymsgr:sendIM?fb.vn01">
+                <img border="0" src="http://opi.yahoo.com/online?u=fb.vn01&amp;m=g&amp;t=1&amp;l=us" class="img-responsive">
+            </a>
+        </div>
+
+          <div class="yahoo">
+            <a href="ymsgr:sendIM?fb.vn02">
+                <img border="0" src="http://opi.yahoo.com/online?u=fb.vn02&amp;m=g&amp;t=1&amp;l=us" class="img-responsive">
+            </a>
+        </div>
+
+          <div class="yahoo">
+            <a href="ymsgr:sendIM?fb.vn03">
+                <img border="0" src="http://opi.yahoo.com/online?u=fb.vn03&amp;m=g&amp;t=1&amp;l=us" class="img-responsive">
+            </a>
+        </div>
+
+          <div class="yahoo">
+            <a href="ymsgr:sendIM?fb.vn04">
+                <img border="0" src="http://opi.yahoo.com/online?u=fb.vn04&amp;m=g&amp;t=1&amp;l=us" class="img-responsive">
+            </a>
+        </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row form-group">
+    <div class="col-md-12">
+      <div class="icon-datve"><i class="fa fa-phone"></i></div>
+      <div class="text-chu-thic">
+        <div class="text-big">Đặt vé qua điện thoại</div>
+        <div class="text-small">
+          <!-- <a href="tel:0873085180">(08) 730.50.180</a> -->
+          <a href="tel:0873050181">(08) 730.50.181</a>
+          <a href="tel:0873050182">(08) 730.50.182</a>
+          <a href="tel:0873085183">(08) 730.50.183</a><br>
+          <a href="tel:0873050500">(08) 730.50.500</a>
+          <a href="tel:0902985355">0902.98.53.55</a>
+          <!-- <a href="tel:0902538556">0902.53.85.86</a> -->
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="row form-group">
+    <div class="col-md-12">
+      <div class="icon-datve"><i class="fa fa-building-o"></i></div>
+      <div class="text-chu-thic">
+        <div class="text-big">Tại văn phòng Flightbooking.vn</div>
+        <div class="text-small">A01, lầu 4, Tòa nhà Mirae Business Center, 268 Tô Hiến Thành <br> P.15, Q.10, TP. Hồ Chí Minh</div>
+      </div>
+    </div>
+  </div>
+
+</div>
