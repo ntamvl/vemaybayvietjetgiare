19c519f463b1cd02314bb426b673038661a801fd
Tam Nguyen <tamnt@zigexn.vn>
2015-05-19 14:35:03 +0700

    update page

--- wp-content/themes/vemaybayvietjetgiare/_search-page.php
+++ wp-content/themes/vemaybayvietjetgiare/_search-page.php
@@ -1,10 +1,3 @@
-<?php get_header(); ?>
-
-<!-- <div class="row featured">
-  <div class="col-sm-12 col-md-12 item-featured">
-
-</div> -->
-
 <div class="col-md-12 cont-grid featured-2" id="search-box-container">
   <div class="panel panel-default panel-search-box">
     <div class="panel-body">
@@ -27,12 +20,6 @@
       jQuery('.footer').addClass('enclose');
     }
 
-    // var isMobile = window.matchMedia("only screen and (max-width: 760px)");
-    // if (isMobile.matches) {
-    //   jQuery('#container-iframe').height($(window).height() - 50);
-    //   jQuery('#sidebar-iframe').height($(window).height() - 50);
-    // }
-
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
       jQuery('.featured-2').height($(window).height() - 50);
     }
@@ -41,5 +28,3 @@
 
   });
 </script>
-
-<?php get_footer(); ?>
