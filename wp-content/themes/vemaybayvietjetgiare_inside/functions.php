84994ea1f457c67c70b2784db7af6fa0fccb98bc
Tam Nguyen <tamnt@zigexn.vn>
2015-05-21 15:45:43 +0700

    update...

--- wp-content/themes/vemaybayvietjetgiare_inside/functions.php
+++ wp-content/themes/vemaybayvietjetgiare_inside/functions.php
@@ -26,7 +26,7 @@
 	add_action( 'pre_get_posts',  'change_posts_number_home_page'  );
 	function change_posts_number_home_page( $query ) {
     if ( is_home() )
-      $query->set( 'posts_per_page', 2 );
+      $query->set( 'posts_per_page', 1 );
 
     return $query;
 	}
