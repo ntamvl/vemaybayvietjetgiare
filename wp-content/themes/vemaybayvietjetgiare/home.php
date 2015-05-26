6cb84b157aeb027c5f8c19ebd9cddd9c6b807376
Tam Nguyen <tamnt@zigexn.vn>
2015-05-26 16:59:07 +0700

    update sidebar

--- wp-content/themes/vemaybayvietjetgiare/home.php
+++ wp-content/themes/vemaybayvietjetgiare/home.php
@@ -128,12 +128,12 @@
 
     $big = 999999999; // need an unlikely integer
 
-    echo paginate_links( array(
-      'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-      'format' => '?paged=%#%',
-      'current' => max( 1, get_query_var('paged') ),
-      'total' => $wp_query->max_num_pages
-    ) );
+    // echo paginate_links( array(
+    //   'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+    //   'format' => '?paged=%#%',
+    //   'current' => max( 1, get_query_var('paged') ),
+    //   'total' => $wp_query->max_num_pages
+    // ) );
     ?>
 
   </div>
